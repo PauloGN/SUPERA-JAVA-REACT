86d86a542ce2c8e5e9092ac82597b2d5722b562d
PauloGN <paulorsantos.gnt@outlook.com>
2022-12-25 10:23:35 -0800

    Desafio 3 Comment and review

--- Desafio 3/src/Main.java
+++ Desafio 3/src/Main.java
@@ -3,21 +3,21 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        //getting external values from args
         int arraySize = Integer.parseInt(args[0]);
         int keyValue = Integer.parseInt(args[1]);
 
         int[] myArray = new int[arraySize];
-
         Random rand = new Random();
 
+        //Filling up the array
         for (int i = 0; i < myArray.length; i++){
-
             myArray[i] = rand.nextInt(11);
-
         }
 
         int amountOfResults = 0;
 
+        //Making comparisons
         for (int i = 0; i < myArray.length; i++){
 
             for (int j = i+1; j < myArray.length; j++ ){
@@ -27,9 +27,8 @@ public class Main {
             }
         }
 
-
+        //Printing up the final result
         System.out.println(amountOfResults);
-
 
     }
 }
