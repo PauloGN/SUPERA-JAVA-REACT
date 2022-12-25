25f05791ac4ddae9f41941ab74cf8d025081ce11
PauloGN <paulorsantos.gnt@outlook.com>
2022-12-24 17:06:10 -0800

    Desafio 1 - Comments and review

--- Desafio 1/src/Main.java
+++ Desafio 1/src/Main.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static boolean isGreater(int n1, int n2){
+    /*
+    Checks two integers and decides if the order must be changed,
+    Numbers must follow even crescent and odd crescent order respectively.
+    */
+    public static boolean SwitchPos(int n1, int n2){
 
-        if (n1%2 == 0 && n2 % 2 != 0 ){
+        if (n1 % 2 == 0 && n2 % 2 != 0 ){
             return false;
         }
 
@@ -14,14 +18,13 @@ public class Main {
         }
 
         return n1 > n2;
-
     }
 
     public static void main(String[] args) {
 
         ArrayList<Integer> numbers = new ArrayList<>();
 
-
+        //just in case of no external arguments
         if(args.length < 1){
 
             System.out.println("Digite 10 numero inteiros: ");
@@ -32,18 +35,19 @@ public class Main {
                 numbers.add(sc.nextInt());
             }
         }
+
+        //In case of external args convert from string to int and save into an array list;
         for (int i = 0; i < args.length; i++){
 
             numbers.add(Integer.parseInt(args[i]));
-
         }
 
-
+        //Simple bubble sort algorithm.
         for (int i = 0; i<numbers.size(); i++){
 
             for (int j = i+1; j<numbers.size(); j++ ) {
 
-                if(isGreater(numbers.get(i), numbers.get(j))){
+                if(SwitchPos(numbers.get(i), numbers.get(j))){
 
                     int temp = numbers.get(i);
                     numbers.set(i, numbers.get(j));
@@ -52,11 +56,10 @@ public class Main {
             }
         }
 
+        //Print final Result
         for (int i = 0; i<numbers.size(); i++){
 
             System.out.println(numbers.get(i));
         }
-
-
     }
 }
